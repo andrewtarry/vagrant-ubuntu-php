3fb005db49f8672658e292b5bdc981a9f09015c7
Andrew Tarry <andrew@andrewtarry.com>
2014-08-06 21:22:11 +0100

    Added fix for php 5.5 install

--- puppet/custom/php/manifests/install.pp
+++ puppet/custom/php/manifests/install.pp
@@ -3,7 +3,7 @@ class php::install {
 
   # The main php package should always be installed so that the cli scripts will work
   package { $php::params::php55_package:
-    ensure => $php::use_version
+    ensure => latest
   }
 
 
